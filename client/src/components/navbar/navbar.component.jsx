011390b26954020cb27802032d2d507fc1df6c66
Piyal Banik <piyalbanik2@gmail.com>
2020-04-24 19:35:30 +0530

    Added Navbar component on client

---
+++ client/src/components/navbar/navbar.component.jsx
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import './navbar.styles.scss';
+
+import {NavLink} from 'react-router-dom';
+import CartIcon from '../cart-icon/cart-icon.component';
+import {ReactComponent as Logo} from '../../assets/crown.svg';
+import {ReactComponent as Account} from '../../assets/account.svg';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <div className = 'navbar'>
+                <NavLink className='logo-container' to='/' >
+                    <Logo className='logo'/>
+                </NavLink>
+
+                <div className='options'>
+                    <NavLink className='option' to='/'>HOME</NavLink>
+                    <NavLink className='option' activeClassName='selected' to='/shop'>SHOP</NavLink>
+                    <NavLink className='option' activeClassName='selected' to='/about'>ABOUT</NavLink>
+                    <NavLink className='option' activeClassName='selected' to='/contact'>CONTACT</NavLink>
+                    <NavLink className='option' activeClassName='selected' to='/account'>MY ACCOUNT</NavLink>
+                    
+                    <CartIcon />
+                </div>
+            </div>
+        );
+    };
+};
+
+export default Navbar;
